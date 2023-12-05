9081b3912964bd8878d9ad7aa1b64c3ad477ad7a
Gan Chin Song <songganjs@gmail.com>
2023-12-05 12:40:01 +0800

    next.js setup

---
+++ src/components/hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <div>
+      <h1 className="text-3xl">Hi I'm Chin Song</h1>
+      <h1>I'm a <span>user-centric</span> developer</h1>
+    </div>
+  );
+}
